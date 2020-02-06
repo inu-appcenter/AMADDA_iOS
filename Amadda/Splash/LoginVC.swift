6cb72d7113bbcb3fa0effc78ccb8ebf2bae45a34
s1gnature <lamant0486@naver.com>
2020-02-06 13:36:32 +0900

    [ADD] SplashVC Init

---
+++ Amadda/Splash/LoginVC.swift
@@ -0,0 +1,16 @@
+//
+//  LoginVC.swift
+//  Amadda
+//
+//  Created by mong on 2020/02/06.
+//  Copyright © 2020 mong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ Amadda/Splash/SplashVC.swift
@@ -0,0 +1,23 @@
+//
+//  SplashVC.swift
+//  Amadda
+//
+//  Created by mong on 2020/02/06.
+//  Copyright © 2020 mong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplashVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        let delayInSeconds = 1.0
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+delayInSeconds){
+            guard let LoginVC = self.storyboard?.instantiateViewController(identifier: "LoginVC") as? LoginVC else{return}
+            self.present(LoginVC, animated: true, completion: nil)
+        }
+    }
+}
