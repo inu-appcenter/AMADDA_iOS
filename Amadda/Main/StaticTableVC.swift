3fbd633bbd7353039a9c6c1c5e84f51014fb2e9b
s1gnature <lamant0486@naver.com>
2020-02-06 15:06:30 +0900

    [ADD] static tableView init

---
+++ Amadda/Main/StaticTableVC.swift
@@ -0,0 +1,14 @@
+//
+//  StaticTableVc.swift
+//  Amadda
+//
+//  Created by mong on 2020/02/06.
+//  Copyright © 2020 mong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StaticTableVC: UITableViewController {
+    
+}
