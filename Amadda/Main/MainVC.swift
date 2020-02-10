1c128ab21a59b30c1e8bdb50e9822db88d896a8a
s1gnature <lamant0486@naver.com>
2020-02-10 20:17:29 +0900

    [ADD] TimeTable 추가

--- Amadda/Main/MainVC.swift
+++ Amadda/Main/MainVC.swift
@@ -9,13 +9,57 @@
 import UIKit
 import Floaty
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    var testData = [    ["1-1","1-2","1-3","1-4","1-5","1-6","1-7","1-8","1-9","1-10","1-11","1-12","1-13","1-14",],["2-1","2-2","2-3","2-4","2-5","2-6","2-7","2-8","2-9","2-10","2-11","2-12","2-13","2-14",],["3-1","3-2","3-3","3-4","3-5","3-6","3-7","3-8","3-9","3-10","3-11","3-12","3-13","3-14",],["4-1","4-2","4-3","4-4","4-5","4-6","4-7","4-8","4-9","4-10","4-11","4-12","4-13","4-14",],["5-1","5-2","5-3","5-4","5-5","5-6","5-7","5-8","5-9","5-10","5-11","5-12","5-13","5-14",]
+    ]
+    
     @IBOutlet var navigationItemBar: UINavigationItem!
-
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var timeLineStackView: UIStackView!
+    
     @IBAction func changeCalendar(_ sender: Any) {
         self.tabBarController?.selectedIndex = 1
     }
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return testData.count
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return testData[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath) as? CustomCell else {return UICollectionViewCell()}
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: cell.frame.width, height: 20))
+        label.text = "\(testData[indexPath.section][indexPath.row])"
+        label.textColor = UIColor.black
+        cell.addSubview(label)
+        
+        cell.backgroundColor = UIColor.white
+        cell.layer.borderColor = UIColor.gray.cgColor
+        cell.layer.borderWidth = 0.3
+        
+        if indexPath == [0,0] || indexPath == [0,1]{
+            let timeTable = UIView(frame: CGRect(x: 0, y: 0, width: cell.frame.width, height: cell.frame.height * 3))
+            timeTable.backgroundColor = UIColor.orange
+            cell.addSubview(timeTable)
+            cell.layer.borderColor = UIColor.gray.cgColor
+            cell.layer.borderWidth = 0.0
+            cell.tag = 1
+        }
+        
+        
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView.cellForItem(at: indexPath)?.tag == 1 {
+            print("item exist")
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width / 5.0, height: collectionView.bounds.height / CGFloat(testData[0].count))
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +71,12 @@ class MainVC: UIViewController {
         dateFormatter.dateFormat = "MMM d일 eeee"
         dateFormatter.locale = Locale(identifier: "ko-KR")
         navigationItemBar.title = dateFormatter.string(from: Date())
+        
+        // MARK: CollectionView (TimeTable)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isScrollEnabled = false
+        collectionView.collectionViewLayout = setLayout(collectionView: collectionView, height: timeLineStackView.frame.height)
         
         // MARK: Floaty
         let floaty = Floaty()
@@ -61,7 +111,14 @@ class MainVC: UIViewController {
         self.view.addSubview(floaty)
                 
     }
-
-
 }
 
+func setLayout(collectionView: UICollectionView, height: CGFloat) -> UICollectionViewLayout {
+    let layout: UICollectionViewFlowLayout =  UICollectionViewFlowLayout()
+    layout.scrollDirection = .horizontal
+    layout.minimumLineSpacing = 0
+    layout.minimumInteritemSpacing = 0
+//    layout.itemSize = CGSize(width: collectionView.frame.width / 5.0, height: height / 14.0)
+//    layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    return layout
+}
