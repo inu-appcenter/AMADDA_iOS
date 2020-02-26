da05b71f008761b6ccedcd39f6d9402f0ece52dd
s1gnature <lamant0486@naver.com>
2020-02-26 14:13:40 +0900

    merge Sidebar

---
+++ Amadda/Sidebar/MenuViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MenuViewController.swift
+//  Amadda
+//
+//  Created by seunghwan Lee on 2020/02/23.
+//  Copyright © 2020 mong. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    var delegate: HomeViewControllerDelegate?
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+}
+    
+
+extension MenuViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "groupCell", for: indexPath) as! GroupCell
+        
+        return cell
+    }
+    
+        
+}
