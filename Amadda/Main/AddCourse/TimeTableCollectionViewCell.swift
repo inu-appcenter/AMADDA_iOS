6e12878786706db63d9ce51a50fb63b428c786cb
s1gnature <lamant0486@naver.com>
2020-03-03 10:04:38 +0900

    [ADD] 수업추가view 시간표 완성

---
+++ Amadda/Main/AddCourse/TimeTableCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TimeTableCollectionViewCell.swift
+//  Amadda
+//
+//  Created by mong on 2020/03/03.
+//  Copyright © 2020 mong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimeTableCollectionViewCell: UICollectionViewCell {
+    
+}
