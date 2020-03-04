8f4ece1f6b3b3733442621287c299d672d6c96d5
shl <tmdghks424@naver.com>
2020-03-04 22:51:53 +0900

    ScheduleListVC 일정 추가 버튼 추가 시그모이드 함수 테스트 이미지 제거 DateFormatter 커스텀 클래스 생성

--- Amadda/Main/AddEvent/AddPersonalEventVC.swift
+++ Amadda/Main/AddEvent/AddPersonalEventVC.swift
@@ -9,16 +9,25 @@
 import Foundation
 import UIKit
 
+
+
 class AddPersonalEventVC: UIViewController {
     
     @IBAction func backBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "personalSegue" {
+            
             guard let AddEventTableVC = segue.destination as? AddEventTableVC else {return}
 //            AddEventTableVC.tableView.cellForRow(at: IndexPath(row: 7, section: 0))?.isHidden = true
             AddEventTableVC.flag = true
         }
     }
+    
+    @IBAction func addBtnPressed(_ sender: Any) {
+        
+    }
+    
 }
